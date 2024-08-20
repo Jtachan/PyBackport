a792a9021452152d0a9d15e619e8e89097077784
Jtachan <76098522+Jtachan@users.noreply.github.com>
2024-08-20 12:12:55 +0200

    Fixed test

--- unittests/test_builtins.py
+++ unittests/test_builtins.py
@@ -3,5 +3,5 @@ from py_back.builtins import str
 
 
 def test_str_functions():
-    my_str: str = "Hello world!"
+    my_str = str("Hello world!")
     assert my_str.removesuffix("!") == "Hello world"
