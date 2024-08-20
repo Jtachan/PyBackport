fd2e4c79a8dd2e74fbf3962348f9fd585fa9d195
Jtachan <76098522+Jtachan@users.noreply.github.com>
2024-08-20 12:01:29 +0200

    Simple test for str (expected fail)

---
+++ unittests/test_builtins.py
@@ -0,0 +1,7 @@
+"""Test the 'builtins' module."""
+from py_back.builtins import str
+
+
+def test_str_functions():
+    my_str: str = "Hello world!"
+    assert my_str.removesuffix("!") == "Hello world"
