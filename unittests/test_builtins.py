1c200195d19ca70cd8998861cf38ea67caa5bd95
Jtachan <76098522+Jtachan@users.noreply.github.com>
2024-09-04 09:10:02 +0200

    Builtins tests

--- unittests/test_builtins.py
+++ unittests/test_builtins.py
@@ -28,10 +28,28 @@ def test_import_warnings():
 @pytest.mark.skipif(
     sys.version_info >= (3, 9), reason="Features included at python 3.9"
 )
-def test_str_methods():
+def test_backported_str():
     """Here are tested all methods backported for the 'str' class."""
     from py_back.builtins import str
+
+    my_str = "Hello world!"
+
+    for backported_method in ("removeprefix", "removesuffix"):
+        assert hasattr(my_str, backported_method) is False
 
     my_str = str("Hello world!")
     assert my_str.removesuffix("!") == "Hello world"
     assert my_str.removeprefix("Hello") == " world!"
+
+
+@pytest.mark.skipif(
+    sys.version_info >= (3, 9), reason="Features included at python 3.9"
+)
+def test_backported_dict():
+    """Here are tested all methods backported for the 'str' class."""
+    from py_back.builtins import dict
+
+    my_dict = dict({"a": 1, "b": 2})
+    result = my_dict | {"c": 3}
+    assert my_dict == {"a": 1, "b": 2}
+    assert result == {"a": 1, "b": 2, "c": 3}
