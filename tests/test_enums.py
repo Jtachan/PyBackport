16c2af36da151d590e1da528e1a9732aa6ad8225
Jtachan <76098522+Jtachan@users.noreply.github.com>
2026-02-12 10:01:16 +0100

    Tests: Enum (updated with new rules)

--- tests/test_enums.py
+++ tests/test_enums.py
@@ -1,27 +1,10 @@
 """Test the 'enums' module."""
 
 import sys
-import warnings
 
 import pytest
 
 
-def test_import_warnings():
-    """Checking the warning is printed for all versions that don't backport the classes."""
-    with warnings.catch_warnings(record=True) as w:
-        warnings.simplefilter("always")
-        from py_back import enum  # noqa: F401
-
-        if sys.version_info >= (3, 11):
-            match_text = "Consider 'from enum import ...' instead of 'from py_back.enum import ...'"
-            assert len(w) == 1, "Expected a warning but none was raised"
-            assert issubclass(w[-1].category, UserWarning)
-            assert match_text in str(w[-1].message)
-        else:
-            assert len(w) == 0
-
-
-@pytest.mark.skipif(sys.version_info >= (3, 11), reason="ReprEnum is available from python 3.11")
 def test_repr_enum():
     """Tests for enum representations.
 
@@ -50,7 +33,6 @@ def test_repr_enum():
     assert str(Number.TWO) == "2"
 
 
-@pytest.mark.skipif(sys.version_info >= (3, 11), reason="StrEnum is available from python 3.11")
 def test_str_enum():
     """Testing py_back.enum.StrEnum."""
     from py_back import enum
